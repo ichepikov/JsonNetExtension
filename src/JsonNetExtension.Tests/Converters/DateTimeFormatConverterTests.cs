7711cf5d7579105fe94bedef7218dbf438b5bd4b
Ivan Chepikov <ivan.chepikov@timetac.com>
2019-01-15 18:28:56 +0100

    Add Tests project
    DateTimeFormatConverterTests was added.

---
+++ src/JsonNetExtension.Tests/Converters/DateTimeFormatConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeepEqual.Syntax;
+using JsonNetExtension.Converters;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace JsonNetExtension.Tests.Converters
+{
+    public class DateTimeFormatConverterTests
+    {
+        [Fact]
+        public void SimpleDateFormatTest()
+        {
+            var jsonString =
+                "{\"id\": 1, \"date\": \"2000-05-01\"}";
+
+            var expectedResult = new TestModel {Id = 1, Date = new DateTime(2000, 05, 01)};
+
+            var result = JsonConvert.DeserializeObject<TestModel>(jsonString);
+
+            expectedResult.ShouldDeepEqual(result);
+        }
+
+        public class TestModel
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+
+            [JsonProperty("date")]
+            [JsonConverter(typeof(DateTimeFormatConverter), "yyyy-MM-dd")]
+            public DateTime Date { get; set; }
+        }
+    }
+}
---
+++ src/JsonNetExtension.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JsonNetExtension.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonNetExtension.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("208a9583-4359-4685-8909-268fb4096776")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
